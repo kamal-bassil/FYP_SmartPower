418ddf78bf8859f4209f941df2b60d3fa04ab249
Kamal <kamal.bassil@lau.edu>
2023-03-02 04:00:25 +0200

    Fixed addLoad, minor bugs with changeSchedule.vue and routing to LoadsDashboard.vue

--- src/views/mainpage.vue
+++ src/views/mainpage.vue
@@ -7,7 +7,7 @@
         <EnergySourcesDashboard :energySources="energySources" />
       </div>
       <div class="loadsContainer">
-        <loadsDashboard :loads="loads" @editClick="this.edit=!this.edit" @load-change-schedule="handleScheduleSubmitted" @delete-load="deleteLoad"/>
+        <loadsDashboard :loads="loads" @editClick="this.edit=!this.edit" @load-change-schedule="handleScheduleSubmitted" @delete-load="deleteLoad" @addNewLoad="addNewLoad"/>
       </div>
     </div>
 
@@ -36,6 +36,7 @@
     created(){
       this.energySources=[{
         id: 1,
+        ipAddress:'192.168.1.10',
         sourceName: 'Battery',
         available:true,
         state:true,
@@ -43,18 +44,21 @@
       },
         {
           id: 2,
+          ipAddress:'192.168.1.11',
           sourceName: 'Solar',
           available:true,
           state:true,
         },
         {
           id: 3,
+          ipAddress:'192.168.1.12',
           sourceName: 'EDL',
           available:true,
           state:false
         },
         {
           id: 4,
+          ipAddress:'192.168.1.13',
           sourceName: 'Subscription generator',
           available:false,
           state:false
@@ -63,9 +67,11 @@
       this.loads=[
           {
         id:1,
+            ipAddress:'192.168.1.31',
         loadName:'Car',
         powerRequirement:3000,
         priority:1,
+        state: true,
         schedule:{
 
           Monday: { start: '12:00', end: '14:00' },
@@ -79,6 +85,7 @@
       },
         {
           id:2,
+          ipAddress:'192.168.1.32',
           loadName:'Hot water',
           powerRequirement:4000,
           priority:1,
@@ -97,11 +104,11 @@
         },
         {
           id:3,
+          ipAddress:'192.168.1.33',
           loadName:'AC',
           powerRequirement:3500,
           priority:2,
-          state:false,
-          scheduled: new Date(2022,10,18,18,0,0,0),
+          state:true,
           schedule:{
 
             Monday: { start: '', end: '' },
@@ -115,11 +122,11 @@
         },
         {
           id:4,
+          ipAddress:'192.168.1.34',
           loadName:'Laundry',
           powerRequirement:500,
           priority:3,
           state:false,
-          scheduled: new Date(2022,10,18,20,0,0,0),
           schedule:{
 
             Monday: { start: '', end: '' },
@@ -150,6 +157,9 @@
         if (indexToDelete !== -1) {
           this.loads.splice(indexToDelete, 1);
         }
+      },
+      addNewLoad(load){
+        this.loads.push(load)
       }
     },
   }
